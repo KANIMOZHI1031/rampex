5f969ec82a20073c5c80d9875263a10116ae0260
KANIMOZHI1031 <kani61118@gmail.com>
2025-05-14 11:07:41 +0530

    Add files via upload

---
+++ training1.c/integer.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+int main() {
+    int a = 10;  // First integer
+    int b = 20;  // Second integer
+    int sum;
+
+    sum = a + b;  // Calculate the sum
+
+    printf("The sum of %d and %d is %d\n", a, b, sum);
+
+    return 0;
+}
